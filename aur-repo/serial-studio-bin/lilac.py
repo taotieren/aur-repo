316f526517e77b156d1650022a8f1bfd916f3f4b
taotieren <admin@taotieren.com>
2026-05-11 16:27:50 +0800

    update serial-studio-bin

--- aur-repo/serial-studio-bin/lilac.py
+++ aur-repo/serial-studio-bin/lilac.py
@@ -3,9 +3,19 @@ from lilaclib import *
 
 def pre_build():
     aur_pre_build('serial-studio-bin', maintainers=['mrinmoy'])
+
+    new_lines = []
     for line in edit_file('PKGBUILD'):
+        if '.AppImage --appimage-extract' in line:
+            indent = '    '
+            new_lines.append(f'{indent}chmod +x ${{_pkgname}}-${{CARCH}}-${{pkgver}}-${{pkgrel}}.AppImage\n')
+            new_lines.append(line)
+        
+        elif 'install -dm644' in line:
+            new_lines.append(line.replace('install -dm644', 'install -dm755'))
+        
+        else:
+            new_lines.append(line)
 
-        if line.strip().endswith('.AppImage --appimage-extract'):
-            print('    chmod +x "${srcdir}"/*.AppImage')
-
-        print(line)
+    with open('PKGBUILD', 'w') as f:
+        f.writelines(new_lines)
